1bcf1b286eaab60155d91887ab7a745f5f83645c
edescalona <ediliodaciel@gmail.com>
2025-01-02 12:43:51 -0500

    [FIX] Enable rental from Inventory, Sales

--- rental_base/models/product.py
+++ rental_base/models/product.py
@@ -1,6 +1,6 @@
 # Part of rental-vertical See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ProductTemplate(models.Model):
@@ -12,4 +12,15 @@ class ProductTemplate(models.Model):
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
-    rental = fields.Boolean("Can be Rent", related="product_tmpl_id.rental", store=True)
+    rental = fields.Boolean(
+        "Can be Rent", compute="_compute_rental", inverse="_inverse_rental", store=True
+    )
+
+    @api.depends("product_tmpl_id.rental")
+    def _compute_rental(self):
+        for product in self:
+            product.rental = product.product_tmpl_id.rental
+
+    def _inverse_rental(self):
+        for product in self:
+            product.product_tmpl_id.rental = product.rental
